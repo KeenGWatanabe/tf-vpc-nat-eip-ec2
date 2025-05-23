e1bd927c0415f42f38e47659633906dbcecaa0c7
KeenGWatanabe <mckeenweb@gmail.com>
2025-05-24 05:47:29 +0800

    r

--- endpoint.tf
+++ endpoint.tf
@@ -4,7 +4,7 @@ resource "aws_vpc_endpoint" "secretsmanager" {
   vpc_id              = aws_vpc.main.id
   service_name        = "com.amazonaws.us-east-1.secretsmanager"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.private.id]  # Your private subnet(s)
+  subnet_ids          = [aws_subnet.private[*].id]  # Your private subnet(s)
   security_group_ids  = [aws_security_group.vpc_endpoint.id]
   private_dns_enabled = true  # Critical for DNS resolution
 }
--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "${var.name_prefix}.tfstate-backend.com"
+    bucket = "ce-grp-4.tfstate-backend.com"
     key = "vpc/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-state-locks"  # Critical for locking
+    dynamodb_table = "ce-grp-4-terraform-state-locks"  # Critical for locking
   }
 }
 
