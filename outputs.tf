2de5bc0a2e5a167a4c5834d827ca0c0eec119a4e
KeenGWatanabe <mckeenweb@gmail.com>
2025-05-25 05:36:33 +0800

    secrets endpoint

--- outputs.tf
+++ outputs.tf
@@ -3,22 +3,6 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-output "vpc_nat" {
-  description = "The NAT Gateway ID"
-  value       = aws_nat_gateway.nat.id
-}
-
-output "vpc_eip" {
-  description = "The Elastic IP associated with NAT"
-  value       = aws_eip.nat.id
-}
-
-
-output "igw_id" {
-  description = "The Internet Gateway ID"
-  value       = aws_internet_gateway.igw.id
-}
-
 output "public_subnets" {
 value = aws_subnet.public[*].id
 }
@@ -26,3 +10,23 @@ value = aws_subnet.public[*].id
 output "private_subnets" {
 value = aws_subnet.private[*].id
 }
+
+output "secretsmanager endpoint" {
+value = aws_vpc_endpoint.secretsmanager.id
+}
+
+# output "vpc_nat" {
+#   description = "The NAT Gateway ID"
+#   value       = aws_nat_gateway.nat.id
+# }
+
+# output "vpc_eip" {
+#   description = "The Elastic IP associated with NAT"
+#   value       = aws_eip.nat.id
+# }
+
+
+# output "igw_id" {
+#   description = "The Internet Gateway ID"
+#   value       = aws_internet_gateway.igw.id
+# }
